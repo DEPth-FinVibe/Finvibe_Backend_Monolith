1eb08e62c541d70367674b73d8f743e7dc855c06
taek <wontaek88@gmail.com>
2026-04-27 14:42:56 +0900

    feat: AsyncConfig에 loadtest 프로파일 추가
    - loadtest 프로파일에서 비동기 작업 실행을 비활성화

--- src/main/java/depth/finvibe/boot/config/investment/AsyncConfig.java
+++ src/main/java/depth/finvibe/boot/config/investment/AsyncConfig.java
@@ -4,11 +4,13 @@ import java.util.concurrent.Executor;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
 @EnableAsync
+@Profile("!loadtest")
 public class AsyncConfig {
 
   @Bean(name = "taskExecutor")
